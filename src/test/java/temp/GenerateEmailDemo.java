d7b00cab00c486bf3f1d309673cef6b8fef318dc
Arun <arun.motoori@gmail.com>
2024-10-24 15:56:46 +0530

    TC_RF_003

--- src/test/java/temp/GenerateEmailDemo.java
+++ src/test/java/temp/GenerateEmailDemo.java
@@ -7,12 +7,13 @@ public class GenerateEmailDemo {
 	public static void main(String[] args) {
 		
 		Date date = new Date();
-		String dateString = date.toString();
-		String noSpaceDateString = dateString.replaceAll("\\s","");
-		String noSpaceAndnoColonsDateString = noSpaceDateString.replaceAll("\\:","");
-		String emailWithTimeStamp = noSpaceAndnoColonsDateString+"@gmail.com";
+		String stringDate = date.toString();
+		String noSpaceStringDate = stringDate.replaceAll("\\s","");
+		String noColonStringDate = noSpaceStringDate.replaceAll("\\:","");
+		String emailWithTimeStamp = noColonStringDate+"@gmail.com";
 		System.out.println(emailWithTimeStamp);
-
+		
+	
 	}
 
 }
